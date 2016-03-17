9de51f80960f5615db0ae46d392882b3f152da29
JD Huntington <jdh@microsoft.com>
2016-03-17 21:26:46 +0000

    More elaborate benchmark.

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,19 +1,27 @@
 var benchrest = require('bench-rest');
+var chanceReq = require('chance');
+var chance = new chanceReq();
 
-
-var data = {
+var dataFn = function() {
+  var data = {
     chirp: {
-        content: "message-#{INDEX}",
-        username:"username-#{INDEX}",
-        loc:[100,-100]
+      content: chance.sentence(),
+      username: chance.name(),
+      loc: [
+        chance.latitude({min: 36.95, max: 48}),
+        chance.longitude({min: -124.45, max: -120.49})
+      ]
     }
+  };
+
+  return data;
 };
 
 var flow = {
-    main: [
-        { post: 'http://104.40.13.35/chirp-api/addChirp', json: data },
-        //        { get: 'http://localhost:8000/foo_#{INDEX}' }
-    ]
+  main: [
+    { post: 'http://104.40.13.35/chirp-api/addChirp', json: dataFn },
+    //        { get: 'http://localhost:8000/foo_#{INDEX}' }
+  ]
 };
 
 // if the above flow will be used with the command line runner or
@@ -23,12 +31,16 @@ module.exports = flow;
 // There are even more flow options like setup and teardown, see detailed usage
 
 var runOptions = {
-    limit: 10,     // concurrent connections
-    iterations: 100  // number of iterations to perform
+  limit: 20,     // concurrent connections
+  iterations: 10000,  // number of iterations to perform
+  progress: 1000
 };
 benchrest(flow, runOptions)
-    .on('error', function (err, ctxName) { console.error('Failed in %s with err: ', ctxName, err); })
-    .on('end', function (stats, errorCount) {
-        console.log('error count: ', errorCount);
-        console.log('stats', stats);
-    });
+  .on('progress', function(statsObj, percent, concurrentCount, ips) {
+    console.log({ ips: ips , percent: percent });
+  })
+  .on('error', function (err, ctxName) { console.error('Failed in %s with err: ', ctxName, err); })
+  .on('end', function (stats, errorCount) {
+    console.log('error count: ', errorCount);
+    console.log('stats', stats);
+  });
